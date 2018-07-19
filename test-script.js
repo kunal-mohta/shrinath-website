d43d03110cd7dc2300d6f7bc4d6924dba32abbf9
kunal-mohta <kunalmohta1818@gmail.com>
2018-07-19 17:39:54 +0530

    rm test-script

--- test-script.js
+++
@@ -1,7 +0,0 @@
-var shell = require('shelljs');
-
-shell.exec('npm run build', function (code) {
-  if (code === 0) {
-    console.log(shell.cp('-r', './build/*', '~/Desktop/tempBuild/').code);
-  }
-});
